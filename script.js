7b6b362c8b2f82ff42ed3de76d56f54f796a51e8
João Pedro Luz <101302893+JoaoLuzWork@users.noreply.github.com>
2026-05-18 15:40:39 +0100

    small modifications to the index.html and script.js files, and renamed an image file. Also, I made some changes to the about.html and hobbies.html files, but those are not staged for commit yet. Additionally, I deleted the footer.js file, which is also not staged for commit. Lastly, I made some modifications to the style.css file, but those changes are not staged for commit either.

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
-
-window.addEventListener("load", function(){
+window.addEventListener("load", function() {
     header.innerHTML = `
         <div class="navBar">
             <div>
                 <H1>Joao Pedro Luz Rodrigues</H1>
             </div>
-            <div class="navItems">
-                <a href="index.html">Home</a> 
-                <a href="about.html">About</a> 
-                <a href="qualifications.html">Qualifications</a> 
-                <a href="hobbies.html">Hobbies</a> 
-            </div>
+            <ul class="navItems">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="hobbies.html">Hobbies</a></li>
+                <li><a href="qualifications.html">Qualifications</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
         </div>`;
 
     footer.innerHTML = `
-        <div class="socialMedias">
-            <a href="https://www.instagram.com/jp_rodriguesluz/"><img class="instaLogo" src="./img/instaLogo.svg" alt=""></a>
-            <a href=""><img class="linkedinLogo" src="./img/linkedin-svgrepo-com.svg" alt=""></a>
-            <a href=""><img class="emailLogo" src="./img/emailLogo.svg" alt=""></a>
-        </div>
-        <div>
-            <P>Joao Pedro Luz Rodrigues 2026</P>
-        </div>
-        `;
-});
+        <div class="footerContent">
+            <div class="socialMedias">
+                <a href="https://www.instagram.com/jp_rodriguesluz/" aria-label="Instagram">
+                    <img class="instaLogo" src="./img/instaLogo.svg" alt="Instagram">
+                </a>
+                <a href="#" aria-label="LinkedIn">
+                    <img class="linkedinLogo" src="./img/linkedin-svgrepo-com.svg" alt="LinkedIn">
+                </a>
+                <a href="mailto:joao.pedro.luz.work@gmail.com" aria-label="Email">
+                    <img class="emailLogo" src="./img/emailLogo.svg" alt="Email">
+                </a>
+            </div>
+            <p>Joao Pedro Luz Rodrigues 2026</p>
+        </div>`;
 
+    const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+    document.querySelectorAll('.navItems a').forEach(link => {
+        if (link.getAttribute('href') === currentPage) {
+            link.classList.add('active');
+        }
+    });
+});
