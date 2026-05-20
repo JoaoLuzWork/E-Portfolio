b5bfd1c470ca8ac382e78b174786f3a7c2e09f3c
João Pedro Luz <101302893+JoaoLuzWork@users.noreply.github.com>
2026-05-20 10:45:42 +0100

    improve button styles and update homepage image, minor images and video adjustments

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ window.addEventListener("load", function() {
             <ul class="navItems">
                 <li><a href="index.html">Home</a></li>
                 <li><a href="about.html">About</a></li>
-                <li><a href="hobbies.html">Hobbies</a></li>
                 <li><a href="qualifications.html">Qualifications</a></li>
+                <li><a href="hobbies.html">Hobbies</a></li>
                 <li><a href="contact.html">Contact</a></li>
             </ul>
         </div>`;
